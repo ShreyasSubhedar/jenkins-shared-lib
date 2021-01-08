0956763b253abc84bad2a251af9ffbcdee2037ed
Charles Moulliard <ch007m@gmail.com>
2021-01-08 15:00:50 +0100

    Include missing silmple quotes

--- vars/renamePomFile.groovy
+++ vars/renamePomFile.groovy
@@ -10,7 +10,7 @@ def call(Map params = [:]){
     def pomBackupFileName = "${pomFileName}${ext}"
 
     println "Backup pom file to ${pomBackupFileName}"
-    def pomXml = readFile(file: ${pomFileName})
+    def pomXml = readFile(file: '${pomFileName}')
     writeFile(file: '${pomBackupFileName}', text: pomXml.text)
 
     println "Remove <dependencyManagement> tag from the pom.xml to allow to control if the GAVs exist"

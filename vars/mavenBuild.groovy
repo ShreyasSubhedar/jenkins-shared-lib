6f65a169ce0be2231bcc12e15eb7884cd1772cfe
Charles Moulliard <ch007m@gmail.com>
2021-01-08 15:44:15 +0100

    Remove stages to execute the mvn commands within the current workspace

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,33 +1,23 @@
-def call(Map params=[:], Closure body={}) {
+def call(Map params = [:]) {
     def compile = params.containsKey('compile') ? params.get('compile') : true
     def test = params.containsKey('test') ? params.get('test') : false
     def packaging = params.containsKey('package') ? params.get('package') : false
     def dependencyTree = params.containsKey('dependencyTree') ? params.get('dependencyTree') : false
 
-    node {
-        stage("Compile") {
-            if (compile) {
-                sh "./mvnw clean compile"
-            }
-        }
-
-        stage("Unit Test") {
-            if (test) {
-                sh "./mvnw test"
-            }
-        }
-
-        stage("Package the project as a Jar") {
-            if (packaging) {
-                sh "./mvnw package -DskipTests=true"
-            }
-        }
-
-        stage("Execute dependency:tree") {
-            if (dependencyTree) {
-                sh "./mvnw dependency:tree"
-            }
-        }
-        body()
+    if (compile) {
+        sh(script: "./mvnw clean compile")
     }
+
+    if (test) {
+        sh(script: "./mvnw test")
+    }
+
+    if (packaging) {
+        sh(script: "./mvnw package -DskipTests=true")
+    }
+
+    if (dependencyTree) {
+        sh(script: "./mvnw dependency:tree")
+    }
+
 }

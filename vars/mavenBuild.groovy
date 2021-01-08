ce9808454a0ccf67242379302ce6798c5c22aa47
Charles Moulliard <ch007m@gmail.com>
2021-01-08 15:38:49 +0100

    Add a new stage to perform a mvn dependency:tree

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -2,6 +2,8 @@ def call(Map params=[:], Closure body={}) {
     def compile = params.containsKey('compile') ? params.get('compile') : true
     def test = params.containsKey('test') ? params.get('test') : false
     def packaging = params.containsKey('package') ? params.get('package') : false
+    def dependencyTree = params.containsKey('dependencyTree') ? params.get('dependencyTree') : false
+
     node {
         stage("Compile") {
             if (compile) {
@@ -15,9 +17,15 @@ def call(Map params=[:], Closure body={}) {
             }
         }
 
-        stage("Package as a Jar") {
+        stage("Package the project as a Jar") {
             if (packaging) {
                 sh "./mvnw package -DskipTests=true"
+            }
+        }
+
+        stage("Execute dependency:tree") {
+            if (dependencyTree) {
+                sh "./mvnw dependency:tree"
             }
         }
         body()

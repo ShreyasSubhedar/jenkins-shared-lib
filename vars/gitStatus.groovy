c24c6a78ea160095ecd6105cd2186872a13680ed
Charles Moulliard <ch007m@gmail.com>
2021-01-08 16:38:27 +0100

    Remove ls -la

--- vars/gitStatus.groovy
+++ vars/gitStatus.groovy
@@ -1,5 +1,5 @@
 def call(Map params = [:]) {
     return sh(label: 'Check git status of the repo',
-            script: 'ls -la && git status',
+            script: 'git status',
             returnStdout: true)
 }

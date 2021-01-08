1159ad5c2bd7b04de9c193d51ecd2e69a7844e45
Charles Moulliard <ch007m@gmail.com>
2021-01-08 14:55:15 +0100

    Rename and replace XML tags

--- vars/renamePomFile.groovy
+++ vars/renamePomFile.groovy
@@ -6,5 +6,18 @@
 
 def call(Map params = [:]){
     def ext = params.containsKey('ext') ? params.ext : ".bk"
-    return ext
+    def pomFileName = "pom.xml"
+    def pomBackupFileName = "${pomFileName}${ext}"
+
+    println "Backup pom file to ${pomBackupFileName}"
+    def pomXml = readFile(file: ${pomFileName})
+    writeFile(file: '${pomBackupFileName}', text: pomXml.text)
+
+    println "Remove <dependencyManagement> tag from the pom.xml to allow to control if the GAVs exist"
+    def pomModified = pomXml.text
+            .replace('<dependencyManagement>', '')
+            .replace('</dependencyManagement>', '')
+    pomXml.text = pomModified
+    writeFile(file: '${pomFileName}', text: pomXml.text)
+    println pomXml.txt
 }

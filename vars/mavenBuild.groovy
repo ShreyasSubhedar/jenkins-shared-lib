b0a2a5b82b1bde839311ac4e0c21944b96bca7f4
Charles Moulliard <ch007m@gmail.com>
2021-01-09 18:18:46 +0100

    Execute the mvn command under a basedir

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -3,27 +3,28 @@ def call(Map params = [:]) {
     def test = params.containsKey('test') ? params.get('test') : false
     def packaging = params.containsKey('package') ? params.get('package') : false
     def dependencyTree = params.containsKey('dependencyTree') ? params.get('dependencyTree') : false
+    def basedir =  params.containsKey('basedir') ? params.basedir : "src"
     def result
 
     if (compile) {
         log(level: 'WARN', text: "mavenBuild: Execute compile goal")
-        sh(script: "mvn clean compile")
+        sh(script: "mvn -f ${basedir}/pom.xml clean compile")
     }
 
     if (test) {
         log(level: 'WARN', text: "mavenBuild: Execute test goal")
-        sh(script: "mvn test")
+        sh(script: "mvn -f ${basedir}/pom.xml test")
     }
 
     if (packaging) {
         log(level: 'WARN', text: "mavenBuild: Execute package goal")
-        sh(script: "mvn package -DskipTests=true")
+        sh(script: "mvn -f ${basedir}/pom.xml package -DskipTests=true")
     }
 
     if (dependencyTree) {
         log(level: 'WARN', text: "mavenBuild: Execute dependency:tree")
         result = sh(script: "mvn -B dependency:tree", returnStdout: true)
-        writeFile(file: "${env.WORKSPACE}/../report.txt", text: result)
+        writeFile(file: "report.txt", text: result)
     }
 
 }

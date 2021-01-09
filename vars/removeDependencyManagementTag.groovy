d3c0a0aace071f85098ea5a4bce683c0540392e6
Charles Moulliard <ch007m@gmail.com>
2021-01-09 18:34:02 +0100

    Add missing basedir

--- vars/removeDependencyManagementTag.groovy
+++ vars/removeDependencyManagementTag.groovy
@@ -6,9 +6,9 @@
 
 def call(Map params = [:]){
     def basedir =  params.containsKey('basedir') ? params.basedir : "src"
-    def pomFileName = "pom.xml"
+    def pomFileName = "${basedir}/pom.xml"
 
-    log(level: 'WARN', text: "Remove <dependencyManagement> tag from the pom.xml to allow to control if the GAVs exist")
+    log(level: 'WARN', text: "Remove <dependencyManagement> tag from the ${basedir}/pom.xml to allow to control if the GAVs exist")
     def pomXml = readFile(file: "${pomFileName}")
     def pomModified = pomXml.replace('<dependencyManagement>', '').replace('</dependencyManagement>', '')
     writeFile(file: "${pomFileName}", text: pomModified)

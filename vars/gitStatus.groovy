859e80431cf4bd62836c643127d2bc9869fe1a9a
Charles Moulliard <ch007m@gmail.com>
2021-01-09 18:40:57 +0100

    Remove .git as non needed

--- vars/gitStatus.groovy
+++ vars/gitStatus.groovy
@@ -1,6 +1,6 @@
 def call(Map params = [:]) {
     def basedir =  params.containsKey('basedir') ? params.basedir : "src"
     return sh(label: 'Check git status of the repo',
-            script: "git -C ${basedir}/.git status",
+            script: "git -C ${basedir} status",
             returnStdout: true)
 }

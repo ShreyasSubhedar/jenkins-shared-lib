0f80954ea739e1364e8846bbddee2c809ae7f8d4
Charles Moulliard <ch007m@gmail.com>
2021-01-09 18:48:50 +0100

    Use change dir

--- vars/gitStatus.groovy
+++ vars/gitStatus.groovy
@@ -1,6 +1,6 @@
 def call(Map params = [:]) {
     def basedir =  params.containsKey('basedir') ? params.basedir : "src"
     return sh(label: 'Check git status of the repo',
-            script: "git -C ${basedir}/ status",
+            script: "cd ${basedir} && git status",
             returnStdout: true)
 }
